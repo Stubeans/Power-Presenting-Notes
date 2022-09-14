019c39e750b7e0bc23041252c3e88172fdadc0cd
JacobBrown1 <gbubjacobb@gmail.com>
2022-09-14 18:25:54 -0400

    retype of Stuart's work on main to testing and addition of Sizegrip element
    git is giving me a headache after switching branches... it should run the same as main right now except we can mouse-over the element of the window that will eventually resize the windows on the bottom right corner (sg.Sizegrip). Stuart wrote all of this already in main branch in case github reads tricky, my original contributions are only line 27 and 61 for now.

--- GuiTest.py
+++ GuiTest.py
@@ -1,38 +1,86 @@
+from fileinput import filename
+from turtle import position
 import PySimpleGUI as sg
-
-counter = 0
-transparencyOptions = [0.75, 0.5, 1]
-
-def writeToFile(myString):
-    f = open("myfile.txt", "w")
-    f = open("myfile.txt", "a")
+def writeToFile(fileName, myString):
+    #f = open(fileName, "w")
+    f = open(fileName, "a")
     f.write(myString)
     f.close()
-    
-sg.theme('Reddit')   # Add a touch of color
-# All the stuff inside your window.
-layout = [  [sg.Text('Some text on Row 1')],
-            [sg.Text('Enter something on Row 2'), sg.InputText()],
-            [sg.Button('Fade'), sg.Button('Read File')],
-            [sg.Button('Ok'), sg.Button('Cancel')],
-            [sg.Text('Debug', key='Debug')]]
 
-# Create the Window
-window = sg.Window('Window Title', layout, keep_on_top = True, finalize = True)
-# Event Loop to process "events" and get the "values" of the inputs
-while True:
-    event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
-        break
-    elif event == 'Ok':
-        print('You entered ', values[0])
-        writeToFile(values[0])
-    elif event == 'Fade':
-        window.set_alpha(transparencyOptions[counter%3])
-        counter += 1
-    elif event == 'Read File':
-        f = open("myFile.txt", "r")
-        text = f.readline()
-        window['Debug'].update(text)
+def readFromFile(fileName):
+    f = open(fileName, "r")
+    text = f.readline()
+    return text
 
-window.close()
+def inputWindow():
+    counter = 0
+    transparencyOptions = [0.75, 0.5, 1]
+
+    sg.theme('Reddit')   # Add a touch of color
+
+    # All the stuff inside your window.
+    layout = [  [sg.Text('Title:'), sg.InputText(size=(30)), sg.Text('Note 1', key='NoteCount'), sg.Button('Add Note')],
+            [sg.Button('<-'), sg.Multiline(key="TextInput", size=(50,20), expand_x=True, expand_y=True), sg.Button('->')],
+            [sg.Button('Fade'), sg.Button('Debug Button')],
+            [sg.Button('Save'), sg.Push(), sg.Button('Read Note'), sg.Button('Close')],
+            [sg.Text('Debug', key='Debug')],
+            [sg.Sizegrip()]  ]
+
+    # Create the Window
+    window = sg.Window('Power Presenting Notes', layout, icon="PPN.ico", keep_on_top = False, finalize = True)
+    # Event Loop to process "events" and get the "values" of the inputs
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED or event == 'Close': # if user closes window or clicks cancel
+            break
+        elif event == 'Save': # saves the value in the text input into a file
+            print('You entered Title: ' + values[0] + ' and contens: ' + values['TextInput'])
+            writeToFile("myfile.txt", values[0] + "!$")
+            writeToFile("myfile.txt", values['TextInput'] + "!$END!$")
+        elif event == 'Fade': # Runs through fade options on a button loop
+            window.set_alpha(transparencyOptions[counter%3])
+            counter += 1
+        elif event == 'Debug Button': # Reads the value from the first line of the file and displays it in the Debug text NOT FUNCTIONAL
+            window['Debug'].update(readFromFile("myfile.txt"))
+        elif event == 'Read Note':
+            window.close()
+            return "Read Note"
+
+    window.close()
+
+def outputWindow():
+    counter = 0
+    transparencyOptions = [0.75, 0.5, 1]
+
+    sg.theme('Reddit')   # Add a touch of color
+
+    # All the stuff inside your window.
+    layout = [  [sg.Text('NOTES:')],
+                [sg.Button('<-'), sg.Output(size=(50, 20)), sg.Button('->')],
+                [sg.Button('Return'), sg.Button('Fade'), sg.Push(), sg.Button('Close')],
+                [sg.Sizegrip()]]
+
+    # Create the Window
+    window = sg.Window('Power Presenting Notes', layout, icon="PPN.ico", keep_on_top = True, finalize = True)
+    # Event Loop to process "events" and get the "values" of the inputs
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED or event == 'Close': # if user closes window or clicks cancel
+            break
+        elif event == 'Return': # Returns to the inputWindow
+            window.close()
+            return "Return"
+        elif event == 'Fade': # Runs through fade options on a button loop
+            window.set_alpha(transparencyOptions[counter%3])
+            counter += 1
+
+    window.close()
+
+def main():
+    print("main")
+    if(inputWindow() == "Read Note"):
+        if(outputWindow() == "Return"):
+            main()
+
+main()
+
