7cd4b0a6421efe79520a46cfe925365d731c3986
Stubeans <stuart.smith2@marist.edu>
2022-10-24 14:50:51 -0400

    Note Reading Capability

--- GuiTest.py
+++ GuiTest.py
@@ -53,13 +53,14 @@ def mainMenu():
             break
         elif event == 'Start':
             window.close()
-            inputWindow()
+            inputWindow("myfile.txt")
 
     window.close()
 
-def inputWindow():
+def inputWindow(file):
     counter = 0
     transparencyOptions = [0.75, 0.5, 1]
+    noteCounter = 0
     
     sg.theme('Reddit')   # Add a touch of color
     
@@ -68,7 +69,7 @@ def inputWindow():
 
     # All the stuff inside your window.
     layout = [  
-        [sg.Text('Title:'), sg.InputText(size=(30)), sg.Text('Note 1', key='NoteCount'), sg.Button('Add Note'), sg.Combo(selection, enable_events=True, key='-COMBO-', default_value= 'Options')],
+        [sg.Text('Title:'), sg.InputText(size=(30), key='title'), sg.Text('Note 1', key='NoteCount'), sg.Button('Add Note'), sg.Combo(selection, enable_events=True, key='-COMBO-', default_value= 'Options')],
         [sg.Button('<-'), sg.Multiline(key="TextInput", size=(50,20), expand_x=True, expand_y=True), sg.Button('->')],
         [sg.Button('Opacity'), sg.Button('Debug Button')],
         [sg.Button('Save'), sg.Push(), sg.Button('Read Note'), sg.Button('Close')],
@@ -80,6 +81,13 @@ def inputWindow():
 
     # Create the Window
     window = sg.Window('Power Presenting Notes', layout, icon="PPN.ico", keep_on_top = False, finalize = True)
+
+    notes = readFromFile(file)
+    window['title'].update(notes[0][0])
+    bodyStr = ""
+    for x in range(len(notes[0])-1):
+        bodyStr += notes[0][x+1]
+    window['TextInput'].update(bodyStr)
     
     # Event Loop to process "events" and get the "values" of the inputs
     while True:
@@ -87,13 +95,13 @@ def inputWindow():
         if event == sg.WIN_CLOSED or event == 'Close': # if user closes window or clicks cancel
             break
         elif event == 'Save': # saves the value in the text input into a file
-            print('You entered Title: ' + values[0] + ' and contents: ' + values['TextInput'])
-            writeToFile("myfile.txt", values[0], values['TextInput'])
+            print('You entered Title: ' + values['title'] + ' and contents: ' + values['TextInput'])
+            writeToFile(file, values['title'], values['TextInput'])
         elif event == 'Fade': # Runs through fade options on a button loop
             window.set_alpha(transparencyOptions[counter%3])
             counter += 1
         elif event == 'Debug Button': # Reads the value from the first line of the file and displays it in the Debug text NOT FUNCTIONAL
-            window['Debug'].update(readFromFile("myfile.txt"))
+            window['Debug'].update(readFromFile(file))
         elif event == "Submit":
             print(values["-IN-"])
         elif event == "Opacity":
@@ -106,20 +114,37 @@ def inputWindow():
                 #print("answer was no")
         elif event == 'Read Note':
             window.close()
-            outputWindow()
+            outputWindow(file)
+        elif event == '<-':
+            if(noteCounter > 0):
+                noteCounter = noteCounter - 1
+                window['NoteCount'].update("Note " + str(noteCounter + 1))
+                window['title'].update(notes[noteCounter][0])
+                bodyStr = ""
+                for x in range(len(notes[noteCounter])-1):
+                    bodyStr += notes[noteCounter][x+1]
+                window['TextInput'].update(bodyStr)
+        elif event == '->':
+            if(noteCounter < len(notes)-1):
+                noteCounter = noteCounter + 1
+                window['NoteCount'].update("Note " + str(noteCounter + 1))
+                window['title'].update(notes[noteCounter][0])
+                bodyStr = ""
+                for x in range(len(notes[noteCounter])-1):
+                    bodyStr += notes[noteCounter][x+1]
+                window['TextInput'].update(bodyStr)
 
     window.close()
 
-def outputWindow():
+def outputWindow(file):
     counter = 0
     transparencyOptions = [0.75, 0.5, 1]
     noteCounter = 0
-    print(noteCounter)
     
     sg.theme('Reddit')   # Add a touch of color
 
     # All the stuff inside your window.
-    layout = [  [sg.Text('NOTES:', key="title")],
+    layout = [  [sg.Text('NOTES:', key="title"), sg.Push(), sg.Text('Note 1', key='NoteCount')],
                 [sg.Button('<-'), sg.Text(size=(50, 20), key="body", background_color="light gray"), sg.Button('->')],
                 [sg.Button('Return'), sg.Button('Fade'), sg.Push(), sg.Button('Close')],
                 [sg.Sizegrip()]]
@@ -127,7 +152,7 @@ def outputWindow():
     # Create the Window
     window = sg.Window('Power Presenting Notes', layout, icon="PPN.ico", keep_on_top = True, finalize = True)
 
-    notes = readFromFile("myfile.txt")
+    notes = readFromFile(file)
     window['title'].update('NOTES: ' + notes[0][0])
     bodyStr = ""
     for x in range(len(notes[0])-1):
@@ -142,13 +167,14 @@ def outputWindow():
             break
         elif event == 'Return': # Returns to the inputWindow
             window.close()
-            inputWindow()
+            inputWindow(file)
         elif event == 'Fade': # Runs through fade options on a button loop
             window.set_alpha(transparencyOptions[counter%3])
             counter += 1
         elif event == '<-':
             if(noteCounter > 0):
                 noteCounter = noteCounter - 1
+                window['NoteCount'].update("Note " + str(noteCounter + 1))
                 window['title'].update('NOTES: ' + notes[noteCounter][0])
                 bodyStr = ""
                 for x in range(len(notes[noteCounter])-1):
@@ -157,6 +183,7 @@ def outputWindow():
         elif event == '->':
             if(noteCounter < len(notes)-1):
                 noteCounter = noteCounter + 1
+                window['NoteCount'].update("Note " + str(noteCounter + 1))
                 window['title'].update('NOTES: ' + notes[noteCounter][0])
                 bodyStr = ""
                 for x in range(len(notes[noteCounter])-1):
